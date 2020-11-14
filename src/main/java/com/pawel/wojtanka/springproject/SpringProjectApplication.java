d000a3cc667d37ac5a881232d4c9742263611b83
Pawel.Wojtanka <pawel.wojtanka@networkedenergy.com>
2020-11-14 08:53:50 +0100

    Init commit

---
+++ src/main/java/com/pawel/wojtanka/springproject/SpringProjectApplication.java
@@ -0,0 +1,13 @@
+package com.pawel.wojtanka.springproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProjectApplication.class, args);
+	}
+
+}
